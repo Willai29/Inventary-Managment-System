c18ffedf66b69609f0c0af344706c98eec88b571
Willai29 <wilmengracelicong@gmail.com>
2026-04-26 20:39:34 +0800

    updating the categories.cs code that it will load all the categories

--- ivs.system/Catagories.cs
+++ ivs.system/Catagories.cs
@@ -17,47 +17,52 @@ namespace ivs.system
         {
             InitializeComponent();
         }
+
         //global varaibles
-        int edit = 0; // this is used indification for save buttton .... what is want to save 
+        int edit = 0;
         short Stat;
-        int CatID;public override void AddBtn_Click(object sender, EventArgs e)
+        int CatID;
+
+        public override void AddBtn_Click(object sender, EventArgs e)
         {
             Mainclass.enable_reset(LeftPanel);
             edit = 0;
-
         }
+
         retrieval re = new retrieval();
 
         private void Catagories_Load(object sender, EventArgs e)
         {
             Mainclass.disable(LeftPanel);
+
+            // ✅ ADDED: Automatically display categories when the form opens
+            re.showCat(Cat_dataGridView, IdGv, NameGv, StsGv);
         }
-        
 
         public override void SaveBtn_Click(object sender, EventArgs e)
         {
             if (Cat_NameTxt.Text == "") { CatErrorLbl.Visible = true; } else { CatErrorLbl.Visible = false; }
             if (StatusDD.SelectedIndex == -1) { StatusErrorLbl.Visible = true; } else { StatusErrorLbl.Visible = false; }
+
             if (StatusDD.SelectedIndex == 0)
             {
-                Stat = 1; // its means user is active in database 1 == Active
+                Stat = 1;
             }
             else if (StatusDD.SelectedIndex == 1)
             {
-                Stat = 0;// its means user is un active in database 0 == unActive
+                Stat = 0;
             }
+
             if (CatErrorLbl.Visible | StatusErrorLbl.Visible)
             {
                 Mainclass.showMsg("All Filed is required", "Caption", "Error");
             }
             else
             {
-                if (edit == 0) // insering new record of user 
+                if (edit == 0)
                 {
-
                     Insertion i = new Insertion();
                     i.insertCat(Cat_NameTxt.Text, Stat);
-
                 }
                 else if (edit == 1)
                 {
@@ -66,12 +71,12 @@ namespace ivs.system
                     {
                         Updatation u = new Updatation();
                         u.updateCat(CatID, Cat_NameTxt.Text, Stat);
-
-
                     }
                 }
+
                 re.showCat(Cat_dataGridView, IdGv, NameGv, StsGv);
             }
+
             Mainclass.disable_reset(LeftPanel);
         }
 
@@ -83,6 +88,7 @@ namespace ivs.system
                 Deletion delRecord = new Deletion();
                 delRecord.deleting(CatID, "st_DeleteCat", "@id");
             }
+
             re.showCat(Cat_dataGridView, IdGv, NameGv, StsGv);
             Mainclass.disable(LeftPanel);
         }
@@ -92,18 +98,18 @@ namespace ivs.system
             Mainclass.enable(LeftPanel);
             edit = 1;
         }
+
         public override void viewBtn_Click(object sender, EventArgs e)
         {
             re.showCat(Cat_dataGridView, IdGv, NameGv, StsGv);
-
             Mainclass.disable_reset(LeftPanel);
         }
+
         public override void searchtxt_TextChanged(object sender, EventArgs e)
         {
-            
             if (searchtxt.Text != "")
             {
-                re.showCat(Cat_dataGridView, IdGv, NameGv, StsGv,searchtxt.Text);
+                re.showCat(Cat_dataGridView, IdGv, NameGv, StsGv, searchtxt.Text);
             }
             else
             {
@@ -111,20 +117,17 @@ namespace ivs.system
             }
         }
 
-        
         private void Cat_dataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-
             if (e.RowIndex != -1)
             {
                 DataGridViewRow row = Cat_dataGridView.Rows[e.RowIndex];
                 CatID = Convert.ToInt32(row.Cells["IdGv"].Value.ToString());
                 Cat_NameTxt.Text = row.Cells["NameGv"].Value.ToString();
                 StatusDD.SelectedItem = row.Cells["StsGv"].Value.ToString();
-
             }
+
             Mainclass.disable(LeftPanel);
         }
-
     }
 }
